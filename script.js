869cebbee3f88a40d24c2ed20f5901ae0169e0f1
Shaili-Patel <526shailipatel@frhsd.com>
2024-12-04 09:30:13 -0500

    Transition

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-let userName = prompt("Hi, who are you?")
+let userName = prompt("Hi, who are you?");
 
-userName ? document.getElementById('greeting').innerText = `Hello ${userName}!`:
+userName ? document.getElementById('greeting').innerText = `Welcome to Glamour 8 Ball \n ${userName}!`:
 document.getElementById('greeting').innerText = 'Hello!'
 
-function shakeMagicEightBall (){
+function shakeMagic8Ball (){
     const randomNumber = Math.floor(Math.random() * 8);
     let eightBall = '';
+    let responseImg;
     
-    let userQuestion = prompt('What is your question for the mighty 8 Ball?')
+    let userQuestion = prompt('What is your question for the mighty 8 Ball?');
     
     switch (randomNumber) {
       case 0:
-      eightBall = 'It is certain';
+      eightBall = 'You may rely on it';
+      responseImg = "imgs/You may rely on it.png";
       break;
       case 1:
-      eightBall = 'It is decidedly so';
+      eightBall = 'It is certain';
+      responseImg = "imgs/It is certain.png";
       break;
-      case 2: eightBall = 'Reply hazy try again';
+      case 2: eightBall = 'My sources say no';
+      responseImg = "imgs/My sources say no.png";
       break;
-      case 3: eightBall = 'Cannot predict right now';
+      case 3: eightBall = 'Yes';
+      responseImg = "imgs/Yes.png";
       break;
-      case 5: eightBall = 'My sources say no';
+      case 5: eightBall = 'Reply hazy, try again later';
+      responseImg = "imgs/Reply hazy.png";
     break;
-    case 6: eightBall='Outlook not so good';
+    case 6: eightBall = 'Without a doubt';
+    responseImg = "imgs/Without a doubt.png";
     break;
-    case 7: eightBall ='Signs point to yes';
-    break;
-    case 8: eightBall = 'Do not count on it';
+    case 7: eightBall = 'Dont count on it';
+    responseImg = "imgs/Don't count on it.png";
     break;
     }
 
-
 console.log(eightBall);
 
-document.getElementById=('response-text').innerText = `${userName} has asked the magic 8 ball ${userQuestion}. The wise and wonderous
+document.getElementById('response-text').innerText = `${userName} has asked the magic 8 ball ${userQuestion}. The wise and wonderous
 8 ball has replied ${eightBall}`
+document.getElementById('response-img').src = responseImg;
 }
